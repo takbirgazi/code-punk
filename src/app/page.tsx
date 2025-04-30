ec6c98b5ca10c2b728eee12306121e2f2124b79d
takbirgazi <takbirgazibd@gmail.com>
2025-05-01 01:18:21 +0600

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Blog from "@/components/HomeComponents/Blog/Blog";
 import CTASection from "@/components/HomeComponents/CTASection/CTASection";
 import FrameworkService from "@/components/HomeComponents/FrameworkService/FrameworkService";
 import Hero from "@/components/HomeComponents/Hero/Hero";
+import Highlights from "@/components/HomeComponents/Highlights/Highlights";
 import Testimonials from "@/components/HomeComponents/Testimonials/Testimonials";
 
 export default function Home() {
@@ -10,7 +11,10 @@ export default function Home() {
       <Hero />
       <div className="max-w-[1250px] mx-auto px-4 pt-10 md:pt-20">
         <FrameworkService />
-        <Testimonials />
+        <Highlights />
+      </div>
+      <Testimonials />
+      <div className="max-w-[1250px] mx-auto px-4 pt-10 md:pt-20">
         <Blog />
         <CTASection />
       </div>
